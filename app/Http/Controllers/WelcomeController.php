9c211456ccbef5aea61cf903b96d0d305bbd7929
mamondebaltob <mhdjang@gmail.com>
2015-06-28 20:59:00 +0900

    줄바꿈 리팩토링

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,13 +2,11 @@
 
 class WelcomeController extends Controller {
 
-	public function __construct()
-	{
+	public function __construct() {
 		$this->middleware('guest');
 	}
 
-	public function index()
-	{
+	public function index() {
 		return view('welcome');
 	}
 
