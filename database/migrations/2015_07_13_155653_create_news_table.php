12f69c4cf87b1cbaf153f712669ac10e5c226555
mamondebaltob <mhdjang@gmail.com>
2015-07-14 01:00:05 +0900

    news 테이블 생성

---
+++ database/migrations/2015_07_13_155653_create_news_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('news', function(Blueprint $table)
+		{
+			$table->increments('id')->unsigned();
+			$table->integer('project_id')->unsigned();
+			$table->foreign('project_id')->references('id')->on('projects');
+			$table->string('title');
+			$table->mediumText('content');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		
+	}
+
+}
