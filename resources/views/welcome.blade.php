0d9165bbd7cfaa9243c432091471db3d05ffa8ab
mamondbaltob <mun@myandb.com>
2015-11-23 02:02:24 +0900

    welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,44 @@
 @extends('app')
 
-@section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+@section('css')
+<style>
+	.ps-welcome-header {
+		padding: 200px 0px 200px 0px;
+		margin-bottom: 40px;
+		background-image: url('{{ asset("/img/app/welcome_header_bg.jpg") }}');
+	}
+	.ps-welcome-header h1 {
+		text-align: center;
+		margin-bottom: 26px;
+		font-family: "Open Sans";
+		font-weight: bold;
+		font-size: 50px;
+	}
+	.ps-welcome-card {
+		width: 700px;
+		padding: 16px;
+		margin: 17px auto 0 auto;
+		text-align: center;
+		background-color: rgba(126, 197, 42, 0.7);
+		color: #FFFFFF;
+		font-weight: bold;
+	}
+</style>
+@endsection
 
-				<div class="panel-body">
-					You are logged in!
-				</div>
-			</div>
-		</div>
+@section('content')
+<div class="first-container ps-welcome-header bg-base">
+	<h1>
+		<span class="text-white">BE ON</span>
+		<span class="text-primary"> STAGE</span>
+	</h1>
+	<img src="{{ asset('/img/app/ico-space.png') }}" class="ico-space" />
+	<div class="ps-welcome-card">
+		안녕하세요 <br/>
+		크라우드 티켓입니당.
 	</div>
+</div>
+<div class="container">
+	@include('template.project', ['projects' => $projects ])
 </div>
 @endsection
