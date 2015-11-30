2032091b8e345179ef46213f520517484e1d5106
mamondbaltob <mun@myandb.com>
2015-11-30 23:14:43 +0900

    뉴스 아이템 디자인

--- resources/views/template/news.blade.php
+++ resources/views/template/news.blade.php
@@ -1,9 +1,15 @@
 <script type="text/template" id="template-news">
-	<li class="list-group-item">
-		<h4><%= data.title %></h4>
+	<li class="list-group-item news-list">
+		<div class="news-title-wrapper">
+			<h5 class="text-ellipsize"><%= data.title %></h5>
+			<span><%= data.created_at %></span>
+		</div>
 		<p><%= data.content %></p>
 		@if ($is_master)
-			<a href="{{ url('/news/') }}/<%= data.id %>/form" class="btn btn-default">수정하기</a>
+		<div class="text-right">
+			<a href="{{ url('/news/') }}/<%= data.id %>/form" class="btn btn-default">수정</a>
+			<a href="{{ url('/news/') }}/<%= data.id %>/form" class="btn btn-default">삭제</a>
+		</div>
 		@endif
 	</li>
 </script>
