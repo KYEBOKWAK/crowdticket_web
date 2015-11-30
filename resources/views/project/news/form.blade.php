babcd9ad098ae7e401a5986c1e00d64bb90025fa
mamondbaltob <mun@myandb.com>
2015-11-30 23:14:36 +0900

    업데이트 작성 페이지 디자인

--- resources/views/project/news/form.blade.php
+++ resources/views/project/news/form.blade.php
@@ -2,44 +2,53 @@
 
 @section('css')
 <link rel="stylesheet" href="{{ asset('/css/editor.css') }}"/>
+<style>
+	.container h2 {
+		font-weight: bold;
+		margin-top: 40px;
+		margin-bottom: 30px;
+	}
+</style>
 @endsection
 
 @section('content')
-<div class="container">
+<div class="first-container container">
 	<div class="row">
-		<div class="col-md-10">
-			<h3>업데이트 작성</h3>
-			<div class="form-horizontal">
-				<div class="form-group">
-					<label for="title" class="col-sm-2 control-label">제목</label>
-					<div class="col-sm-8">
+		<h2 class="text-center">업데이트 작성</h2>
+		<div class="col-md-10 col-md-offset-1">
+			<div class="row">
+				<div class="col-md-10">
+					<div class="form-group">
+						<label for="title" class="control-label">제목</label>
 						@if ($news)
-							<input id="title" type="text" name="title" class="form-control" value="{{ $news->title }}" />
+						<input id="title" type="text" name="title" class="form-control" value="{{ $news->title }}" />
 						@else
-							<input id="title" type="text" name="title" class="form-control" />
+						<input id="title" type="text" name="title" class="form-control" />
 						@endif
 					</div>
 				</div>
-				<div class="form-group">
-					<label class="col-sm-2 control-label">내용</label>
-					<div class="col-sm-10">
-						<input type="hidden" id="tx_image_params" value="url={{ url() }}/projects/{{ $project->id }}/news/images&csrf_token={{ csrf_token() }}" />
-						@if ($news)
-							<textarea id="tx_load_content" style="display: none">{{ $news->content }}</textarea>
-						@endif
-						@include('editor')
-					</div>
-				</div>
-				@if ($news)
-					<button id="update_news" class="btn btn-success pull-right">수정하기</button>
-					<input type="hidden" id="method" value="put" />
-				@else
-					<button id="update_news" class="btn btn-success pull-right">만들기</button>
-					<input type="hidden" id="method" value="post" />
-				@endif
-				<input type="hidden" id="ajax_url" value="{{ $ajax_url }}" />
-				<input type="hidden" id="project_id" value="{{ $project->id }}" />
 			</div>
+			<div class="form-group">
+				<label class="control-label">내용</label>
+				<input type="hidden" id="tx_image_params" value="url={{ url() }}/projects/{{ $project->id }}/news/images&csrf_token={{ csrf_token() }}" />
+				@if ($news)
+				<textarea id="tx_load_content" style="display: none">{{ $news->content }}</textarea>
+				@endif
+				@include('editor')
+			</div>
+			<div class="row">
+				<div class="col-md-12 text-center">
+					@if ($news)
+					<button id="update_news" class="btn btn-success">수정하기</button>
+					<input type="hidden" id="method" value="put" />
+					@else
+					<button id="update_news" class="btn btn-success">작성하기</button>
+					<input type="hidden" id="method" value="post" />
+					@endif
+				</div>
+			</div>
+			<input type="hidden" id="ajax_url" value="{{ $ajax_url }}" />
+			<input type="hidden" id="project_id" value="{{ $project->id }}" />
 		</div>
 	</div>
 </div>
