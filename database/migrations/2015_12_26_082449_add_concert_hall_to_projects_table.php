505914341c0a8526fb80bb9270d49b76ff2d8ff9
mamondbaltob <mun@myandb.com>
2015-12-26 17:28:06 +0900

    공연장 칼럼 추가

---
+++ database/migrations/2015_12_26_082449_add_concert_hall_to_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddConcertHallToProjectsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('projects', function(Blueprint $table)
+		{
+			$table->string('concert_hall')->default('')->after('detailed_address');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('projects', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
