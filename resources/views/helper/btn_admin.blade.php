16607db6c8778505a3f48708bdb4b000d91425bb
mamondbaltob <mun@myandb.com>
2015-12-26 16:04:47 +0900

    프로젝트 어드민 버튼 메뉴 텍스트 수정

--- resources/views/helper/btn_admin.blade.php
+++ resources/views/helper/btn_admin.blade.php
@@ -2,8 +2,8 @@
 <div class="btn-admin dropdown">
 	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><img src="{{ asset('img/app/btn_admin.png') }}" /></a>
 	<ul class="dropdown-menu" role="menu">
-		<li><a href="{{ url('/projects') }}/{{ $project->id }}" >상세보기</a></li>
-		<li><a href="{{ url('/projects') }}/form/{{ $project->id }}" >프로젝트 관리</a></li>
+		<li><a href="{{ url('/projects') }}/{{ $project->id }}" >프로젝트 보기</a></li>
+		<li><a href="{{ url('/projects') }}/form/{{ $project->id }}" >프로젝트 수정</a></li>
 		<li><a href="{{ url('/projects') }}/{{ $project->id }}/orders" >후원자 관리</a></li>
 	</ul>
 </div>
