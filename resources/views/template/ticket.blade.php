1ed8f87c304d3615fc7a6485e6da579f77ea338d
mamondbaltob <mun@myandb.com>
2016-02-09 16:50:53 +0900

    ubuntu 서버에서 php integer 가 string 으로

--- resources/views/template/ticket.blade.php
+++ resources/views/template/ticket.blade.php
@@ -70,7 +70,7 @@
 		</a>
 		<% } %>
 		
-		<% if (style === 'modifyable' && ticket.audiences_count === 0) { %>
+		<% if (style === 'modifyable' && parseInt(ticket.audiences_count) === 0) { %>
 		<div class="col-md-1">
 			<p>
 				<button class="btn btn-primary modify-ticket">수정</button>
