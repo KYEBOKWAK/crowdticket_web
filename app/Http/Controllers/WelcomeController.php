39d9ca11e0e04dc5f967cf40c97273f7c12e813d
mamondbaltob <mhdjang@gmail.com>
2016-03-08 20:29:50 +0900

    홈 6개 마감안된거 최신순으로 갖고오기

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,8 +3,13 @@
 class WelcomeController extends Controller {
 
 	public function index() {
+		$now = date('Y-m-d H:i:s');
+		
 		return view('welcome', [
-			'projects' => \App\Models\Project::where('state', 4)->take(3)->get()
+			'projects' => \App\Models\Project::where('state', 4)
+					->where('funding_closing_at', '>', $now)
+					->orderBy('id', 'desc')
+					->take(6)->get()
 		]);
 	}
 
