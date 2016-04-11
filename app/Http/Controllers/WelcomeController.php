3be0522a23d93dac8b0db2f7fbbfabe7f867af29
mamondbaltob <mhdjang@gmail.com>
2016-04-11 20:59:39 +0900

    홈에 공연 최소6개 디피

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -5,11 +5,24 @@ class WelcomeController extends Controller {
 	public function index() {
 		$now = date('Y-m-d H:i:s');
 		
-		return view('welcome', [
-			'projects' => \App\Models\Project::where('state', 4)
+		$minExposedNum = 6;
+		$projects = \App\Models\Project::where('state', 4)
 					->where('funding_closing_at', '>', $now)
 					->orderBy('id', 'desc')
-					->take(6)->get()
+					->take($minExposedNum)->get();
+		
+		$lack = $minExposedNum - count($projects);
+		if ($lack > 0) {
+			$additional = \App\Models\Project::where('state', 4)
+					->where('funding_closing_at', '<', $now)
+					->orderBy('id', 'desc')
+					->take($lack)->get();
+			
+			$projects = $projects->merge($additional); 
+		} 
+		
+		return view('welcome', [
+			'projects' => $projects
 		]);
 	}
 
