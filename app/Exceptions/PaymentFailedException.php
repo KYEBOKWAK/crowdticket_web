df98d616a136f865437c34eb23270704d4f1bff3
mamondbaltob <mhdjang@gmail.com>
2016-05-22 23:35:52 +0900

    아임포트 예외 추가

---
+++ app/Exceptions/PaymentFailedException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mhd
+ * Date: 2016-05-17
+ * Time: 오후 11:16
+ */
+
+namespace App\Exceptions;
+
+
+class PaymentFailedException extends \Exception
+{
+
+}
