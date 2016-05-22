840cfea8c274124721bf13a3bbead9d8d28afc95
mamondbaltob <mhdjang@gmail.com>
2016-05-23 03:01:55 +0900

    비밀번호 초기화 세팅

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'contact@crowdticket.kr', 'name' => '크라우드티켓'],
 
 	/*
 	|--------------------------------------------------------------------------
