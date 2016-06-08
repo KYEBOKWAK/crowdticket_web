d9968e6536f5df683868a130e5bf05658b7942d3
mamondbaltob <mhdjang@gmail.com>
2016-06-08 22:56:34 +0900

    결제 완료 페이지 문구 변경

--- resources/views/order/complete.blade.php
+++ resources/views/order/complete.blade.php
@@ -10,10 +10,11 @@
             margin-bottom: 40px;
         }
 
-        .ps-info {
-            margin-top: 40px;
-            margin-bottom: 40px;
-            font-size: 14px;
+        .ps-text-detail {
+            margin-top: 5em;
+        }
+        .ps-text-detail strong {
+            font-size: 1.2em;
         }
     </style>
 @endsection
@@ -24,12 +25,23 @@
         <div class="row ps-box">
             <div class="col-md-12">
                 <h2 class="text-center"><strong>THANK YOU!</strong></h2>
-                <h3 class="text-center"><strong>참여가 완료되었습니다.</strong></h3>
-
-                @if ($order->price > 0)
-                    <div class="text-center text-danger">
-                        오른쪽 상단 '결제확인' 탭에서 관리하세요!
-                    </div>
+                @if ($project->type === 'funding')
+                    <h3 class="text-center"><strong>결제가 성공적으로 예약되었습니다.</strong></h3>
+                    <p class="text-center text-danger">입력하신 정보는 철저히 암호화 된 상태로 안전하게 다뤄집니다.</p>
+                    <p class="text-center ps-text-detail">
+                        <strong>
+                            펀딩이 성공하면, '완료일'에 모든 후원자의 결제가 일괄 진행됩니다.<br/>
+                            펀딩이 실패하면 결제되지 않습니다.<br/><br/>
+                        </strong>
+                        결제예약은 오른쪽 상단 '결제확인' 탭에서 취소할 수 있으며,<br/>
+                        취소하면 펀딩의 후원이 자동 취소됩니다.<br/>
+                        단, 펀딩이 완료되기 24시간 전부터는 취소할 수 없습니다.
+                    </p>
+                @else
+                    <h3 class="text-center"><strong>결제가 완료되었습니다.</strong></h3>
+                    <p class="text-center ps-text-detail">
+                        <strong>오른쪽 상단 '결제확인' 탭에서 확인해보세요!</strong>
+                    </p>
                 @endif
             </div>
         </div>
