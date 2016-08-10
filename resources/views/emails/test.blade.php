bd46d9319d3e94a7250e1f31d0dd8c0efdb463cf
mamondbaltob <mhdjang@gmail.com>
2016-08-11 01:15:48 +0900

    이미지 경로 버그

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -1,4 +1,4 @@
-<img class="logo" src="{{ 'img/app/logo_new.png' }}" style="display: block; margin: 0 auto 1em auto;max-width: 15%;"/>
+<img class="logo" src="{{ asset('img/app/logo_new.png') }}" style="display: block; margin: 0 auto 1em auto;max-width: 15%;"/>
 <div style="max-width: 95%; margin: 0 auto;">
     <p style="font-size: 14px; color: #222; margin: 0 0 1em 0.5em;">{{ $title }}</p>
     <table width="100%" border="1px solid black" cellpadding="8px" cellspacing="0" style="border-collapse: collapse;color: #333;font-size: 13px;">
