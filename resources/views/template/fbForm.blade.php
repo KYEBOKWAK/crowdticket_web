e1d6ece2c95433899b7dabb3f0b749b1a3e05e96
곽계보 <bogame@crowdticket.kr>
2018-07-11 15:22:28 +0900

    facebook 공유하기 취소 버튼 눌럿을때 공유 성공 팝업 버그 수정

--- resources/views/template/fbForm.blade.php
+++ resources/views/template/fbForm.blade.php
@@ -6,10 +6,11 @@ document.getElementById('BtnFBshare').onclick = function() {
     href: 'https://crowdticket.kr/projects/{{ $project->id }}',
     hashtag: '#크라우드티켓'
   }, function(response){
-    alert(response.error_code);
-
     if (response && !response.error_code) {
-              //swal("공유 성공!", "예술인에 한발짝 더 다가갔습니다!", "success");
+      if(response.error_code != 4201)//4201 code: 유저가 cancel 했을때 
+      {
+        swal("공유 성공!", "예술인에 한발짝 더 다가갔습니다!", "success");
+      }
     } else {
     }
   });
