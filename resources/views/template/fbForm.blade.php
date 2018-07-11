f0825baddd37d0d4c3cd4ab0a1e14bd21828110c
곽계보 <bogame@crowdticket.kr>
2018-07-11 15:03:59 +0900

    facebook 공유하기 버그 수정  - 모바일로 닫기시 성공 팝업 뜨는 버그 수정

--- resources/views/template/fbForm.blade.php
+++ resources/views/template/fbForm.blade.php
@@ -6,13 +6,10 @@ document.getElementById('BtnFBshare').onclick = function() {
     href: 'https://crowdticket.kr/projects/{{ $project->id }}',
     hashtag: '#크라우드티켓'
   }, function(response){
-    if(response) {
-      swal("공유 성공!", "예술인에 한발짝 더 다가갔습니다!", "success");
+    if (response && !response.error_code) {
+              swal("공유 성공!", "예술인에 한발짝 더 다가갔습니다!", "success");
+    } else {
     }
-    else {
-      //값이 있으면 error 혹은 닫기 버튼 누름
-    }
-
   });
 }
 </script>
