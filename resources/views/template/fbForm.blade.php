df66142b3f5e5e7b983970df69ad1185d5123618
곽계보 <bogame@crowdticket.kr>
2018-07-11 15:15:48 +0900

    facebook 에러코드 테스트

--- resources/views/template/fbForm.blade.php
+++ resources/views/template/fbForm.blade.php
@@ -6,8 +6,10 @@ document.getElementById('BtnFBshare').onclick = function() {
     href: 'https://crowdticket.kr/projects/{{ $project->id }}',
     hashtag: '#크라우드티켓'
   }, function(response){
+    alert(response.error_code);
+
     if (response && !response.error_code) {
-              swal("공유 성공!", "예술인에 한발짝 더 다가갔습니다!", "success");
+              //swal("공유 성공!", "예술인에 한발짝 더 다가갔습니다!", "success");
     } else {
     }
   });
