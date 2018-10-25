752e3b458613b9bd018a992619f8d6b8af1b2681
KYEBOKWAK <bogame@naver.com>
2018-10-25 16:46:58 +0900

    admin 버튼 캐싱 문제 수정

--- resources/views/helper/btn_admin.blade.php
+++ resources/views/helper/btn_admin.blade.php
@@ -1,7 +1,7 @@
 @if (\Auth::check() && \Auth::user()->isOwnerOf($project))
     <div class="btn-admin dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><img
-                    src="{{ asset('img/app/btn_admin.png') }}"/></a>
+                    src="{{ asset('img/app/btn_admin_1.png') }}"/></a>
         <ul class="dropdown-menu" role="menu">
             <li><a href="{{ url('/projects') }}/{{ $project->id }}">프로젝트 보기</a></li>
             <li><a href="{{ url('/projects') }}/form/{{ $project->id }}">프로젝트 수정</a></li>
