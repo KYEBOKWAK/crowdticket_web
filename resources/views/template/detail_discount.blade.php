16a4920c1747d563a51cb69faecdccb743f5d34b
KYEBOKWAK <bogame@naver.com>
2018-11-01 17:50:06 +0900

    할인 표시 수정

--- resources/views/template/detail_discount.blade.php
+++ resources/views/template/detail_discount.blade.php
@@ -1,5 +1,5 @@
-<h5>{{ $discount->content }} {{ $discount->percent_value }}원 할인</h5>
-<h5 style="font-weight:400; color:#656969;">{{ $discount->submit_check }}.확인이 안 될 시 차액을 지급해야 합니다.</h5>
+<h5>{{ $discount->content }} {{ $discount->percent_value }}% 할인</h5>
+<h5 style="font-weight:400; color:#656969;">{{ $discount->submit_check }}.</h5>
 
 <!-- <h5>중고등학생 2000원 할인</h5>
 <h5 style="font-weight:400; color:#656969;">현장에서 학생증을 제시하셔야 합니다.확인이 안 될 시 차액을 지급해야 합니다.</h5> -->
