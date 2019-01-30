2481092e6b4ca9ad5cd0fc69cff0922d697b3f5e
KYEBOKWAK <bogame@naver.com>
2019-01-30 16:27:12 +0900

    디테일 페이지 티켓란 높이 버그 수정

--- resources/views/project/detail_renew.blade.php
+++ resources/views/project/detail_renew.blade.php
@@ -10,7 +10,7 @@
 @endsection
 
 @section('css')
-    <link href="{{ asset('/css/detail.css?version=13') }}" rel="stylesheet">
+    <link href="{{ asset('/css/detail.css?version=14') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('/css/goods.css?version=5') }}"/>
     <link rel="stylesheet" href="{{ asset('/css/project/form_body_ticket.css?version=3') }}"/>
     <style>
