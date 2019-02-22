3ddf1747ec4d5bb81a404f425b9fd6571232bdd6
KYEBOKWAK <bogame@naver.com>
2019-02-22 22:41:01 +0900

    서버

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -34,7 +34,7 @@
 <body>
 <div class="container">
     <div class="content">
-        <div class="title" style="font-size: 50px;">크라우드티켓 서버 개선중..<br> 약 5분 뒤 정상화 됩니다. <br> 조금만 기다려주세요 :)</div>
+        <div class="title" style="font-size: 50px;">크라우드티켓 서버 개선중..<br> 2월 23일 오전9시 오픈 예정 <br> 더 좋은 모습으로 찾아뵙겠습니다. :)</div>
     </div>
 </div>
 </body>
