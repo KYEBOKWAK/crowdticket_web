59205c403031903d25e91f43d89a5266063a413d
KYEBOKWAK <bogame@naver.com>
2019-04-01 19:29:36 +0900

    리디렉션 복구

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -1,15 +1,8 @@
 <?php
-
+/*
 if ( ! function_exists('url'))
 {
-	/**
-	 * Generate a url for the application.
-	 *
-	 * @param  string  $path
-	 * @param  mixed   $parameters
-	 * @param  bool    $secure
-	 * @return string
-	 */
+
 	function url($path = null, $parameters = array(), $secure = null)
 	{
     $secure = true;
@@ -17,20 +10,14 @@ if ( ! function_exists('url'))
     {
       $secure = false;
     }
-    
+
 		return app('Illuminate\Contracts\Routing\UrlGenerator')->to($path, $parameters, $secure);
 	}
 }
 
 if ( ! function_exists('asset'))
 {
-	/**
-	 * Generate an asset path for the application.
-	 *
-	 * @param  string  $path
-	 * @param  bool    $secure
-	 * @return string
-	 */
+	
 	function asset($path, $secure = null)
 	{
     $secure = true;
@@ -42,5 +29,5 @@ if ( ! function_exists('asset'))
 		return app('url')->asset($path, $secure);
 	}
 }
-
+*/
 ?>
