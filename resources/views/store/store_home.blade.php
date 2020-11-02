8b4d257485ad536731d4140da6c835fd2eb0e5af
GyeBo Gwak <bogame@naver.com>
2020-11-02 11:50:01 +0900

    스토어 홈 오픈그래프 추가

--- resources/views/store/store_home.blade.php
+++ resources/views/store/store_home.blade.php
@@ -1,7 +1,10 @@
 @extends('app')
 
 @section('meta')
-    <meta property="og:image" content="{{ asset('/img/app/og_image_3.png') }}"/>
+  <meta property="og:type" content="website"/>
+  <meta property="og:title" content="크티 | 콘텐츠 상점"/>
+  <meta property="og:description" content="내가 좋아하는 크리에이터가 만들어주는 나만을 위한 콘텐츠, 크티 콘텐츠 상점"/>
+  <meta property="og:image" content="{{ asset('/img/app/og_image_3.png') }}"/>  
 @endsection
 
 @section('title')
