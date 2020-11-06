89d5ee4325fe6e6b05ea8907db314954e9d0f10e
GyeBo Gwak <bogame@naver.com>
2020-11-06 15:53:46 +0900

    반려 확인 버튼 텍스트 하단으로 나오는 이슈 수정

--- resources/views/store/store_detail_receipt.blade.php
+++ resources/views/store/store_detail_receipt.blade.php
@@ -15,7 +15,7 @@
 rel="stylesheet"/>
 <link href="{{ asset('/dist/css/StoreOrderItem.css?version=1') }}" 
 rel="stylesheet"/>
-<link href="{{ asset('/dist/css/StoreReceiptItem.css?version=5') }}" 
+<link href="{{ asset('/dist/css/StoreReceiptItem.css?version=6') }}" 
 rel="stylesheet"/>
 
 @endsection
