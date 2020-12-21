62c3643610cb22c36bf4453139e227e69eb946b7
GyeBo Gwak <bogame@naver.com>
2020-12-21 18:09:52 +0900

    이벤트 페이지 메타데이터 추가

--- resources/views/events/event_page.blade.php
+++ resources/views/events/event_page.blade.php
@@ -5,6 +5,7 @@
     <meta property="og:title" content="크티 | 이벤트"/>
     <meta property="og:description" content="내가 좋아하는 크리에이터의 나만을 위한 콘텐츠, 콘텐츠 상점"/>
     <meta property="og:image" content="{{ asset('/img/app/og_image_3.png') }}"/>
+    <meta name="description" content="내가 좋아하는 크리에이터의 나만을 위한 콘텐츠, 콘텐츠 상점"/>
 @endsection
 
 @section('title')
