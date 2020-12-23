020d035477f9bf43a6539aaaa59f281e496553cf
GyeBo Gwak <bogame@naver.com>
2020-12-23 18:27:55 +0900

    이벤트 페이지 og image 변경

--- resources/views/events/event_page.blade.php
+++ resources/views/events/event_page.blade.php
@@ -5,7 +5,7 @@
     <meta property="og:type" content="website"/>
     <meta property="og:title" content="크티 | 이벤트"/>
     <meta property="og:description" content="내가 좋아하는 크리에이터의 나만을 위한 콘텐츠, 콘텐츠 상점"/>
-    <meta property="og:image" content="{{ asset('/img/app/og_image_3.png') }}"/>
+    <meta property="og:image" content="{{ asset('/img/app/og_image_4.png') }}"/>
     <meta name="description" content="내가 좋아하는 크리에이터의 나만을 위한 콘텐츠, 콘텐츠 상점"/>
 @endsection
 
