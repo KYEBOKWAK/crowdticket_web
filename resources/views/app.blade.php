7914575c5a43f83f8d8974fb88b45b99dc38fe88
GyeBo Gwak <bogame@naver.com>
2021-03-04 18:13:12 +0900

    테스트 원복

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -674,7 +674,7 @@ function logout(){
     }
 </script>
 
-<script type="text/javascript" src="{{ asset('/dist/App.js?version=132') }}"></script>
+<script type="text/javascript" src="{{ asset('/dist/App.js?version=133') }}"></script>
 
 </body>
 </html>
