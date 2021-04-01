c7a924c4a115937f339597c52647e17ff2f91561
GyeBo Gwak <bogame@naver.com>
2021-04-01 13:47:55 +0900

    자음모음 분리 현상 확인 로그

--- resources/views/app_back_2021_02_15.blade.php
+++ resources/views/app_back_2021_02_15.blade.php
@@ -626,7 +626,7 @@ function logout(){
     }
 </script>
 
-<!-- <script type="text/javascript" src="{{ asset('/dist/App.js?version=155') }}"></script> -->
+<!-- <script type="text/javascript" src="{{ asset('/dist/App.js?version=156') }}"></script> -->
 
 </body>
 </html>
